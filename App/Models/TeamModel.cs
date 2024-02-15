66bccd53faeba215b8254e6ac00cec3033dea8bc
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-02-15 11:32:18 +0100

    create model

---
+++ App/Models/TeamModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class TeamModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Display(Name = "Pos")]
+        public byte League_Position { get; set; }
+
+        [Required]
+        [Display(Name = "Team")]
+        public string Name { get; set; }
+
+        [Display(Name = "Pld")]
+        public byte Matches_Played { get; set; }
+
+        [Display(Name = "Pts")]
+        public sbyte Points { get; set; }
+
+        [Display(Name = "GD")]
+        public sbyte Goal_Diff { get; set; }
+
+        [Display(Name = "W")]
+        public byte Wins { get; set; }
+
+        [Display(Name = "D")]
+        public byte Draws { get; set; }
+
+        [Display(Name = "L")]
+        public byte Losts { get; set; }
+    }
+}
