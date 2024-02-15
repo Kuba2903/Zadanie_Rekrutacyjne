a46b805b68dba38a8df63cc1e5b5776bd4cb2752
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-02-15 10:52:49 +0100

    create entity

---
+++ Data/Entities/TeamEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("teams")]
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        public byte League_Position { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public byte Matches_Played { get; set; }
+
+        public byte Points { get; set; }
+
+        public byte Goal_Diff { get; set; }
+
+        public byte Wins { get; set; }
+
+        public byte Draws { get; set; }
+
+        public byte Losts { get; set; }
+
+
+    }
+}
